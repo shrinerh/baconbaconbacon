c009036ab4030ddbd4fa2caf9a8bdebd952961a0
Hans <shrinerh@gmail.com>
2026-01-26 19:35:18 -0500

    Add project files.

---
+++ baconbaconbacon/baconbaconbaconModSystem.cs
@@ -0,0 +1,27 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace baconbaconbacon
+{
+    public class baconbaconbaconModSystem : ModSystem
+    {
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("baconbaconbacon:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("baconbaconbacon:hello"));
+        }
+    }
+}
