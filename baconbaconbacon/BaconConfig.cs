6b71aaab71238bb39e6297c3fe73bf0e80600146
Hans <shrinerh@gmail.com>
2026-01-26 23:04:29 -0500

    Add very sarcastic config to turn off bacon crops

---
+++ baconbaconbacon/BaconConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace baconbaconbacon
+{
+    public class BaconConfig
+    {
+        // Disable Bacon Crop Stuff
+        public bool TurnOnBaconCrops { get; set; } = true;
+        public bool TurnOffBaconCrops { get; set; } = false;
+        public bool DefinitelyTurnOffBaconCrops { get; set; } = false;
+        public bool GrowableBacon { get; set; } = true;
+        public bool DoNotUseBaconCrops { get; set; } = false;
+        public bool TurnOffFun { get; set; } = false;
+        public bool TurnOffWhimsey { get; set; } = false;
+        public bool TurnOnChildlikeWondermentRecapturedAsAnAdult { get; set; } = true;
+        public string SetThisValueToThePasswordOnTheModPageIfYouWantToTurnOffGrowableBacon { get; set; } = "";
+        public bool SeriouslyActuallyTurnOffTheWeirdBaconCrops { get; set; } = false;
+        public bool ISincerelyDontWantTheCabbageBacon { get; set; } = false;
+        public bool AreYouGettingTiredOfThisYetBecauseIfSoSetThisOneToTrue { get; set; } = false;
+        public bool GrowingBaconIsLame { get; set; } = false;
+        public bool TurnOnHumor { get; set; } = true;
+        public int SetThisToTheAnswerToLifeTheUniverseAndEverythingToTurnOffBaconCrops { get; set; } = 69;
+        public bool MeatableCrops { get; set; } = true;
+        public bool CroppableMeats { get; set; } = true;
+        public bool BaconFarmingMechanics { get; set; } = true;
+        public bool SlavishDevotionToRealism { get; set; } = false;
+        public int SetThisToTheIntegerNumberOneThousandTwoHundredAndTwentyFiveToTurnOffBaconCrops { get; set; } = 420;
+
+    }
+}
--- baconbaconbacon/baconbaconbaconModSystem.cs
+++ baconbaconbacon/baconbaconbaconModSystem.cs
@@ -1,4 +1,5 @@
-﻿using Vintagestory.API.Client;
+﻿using System.Linq;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
@@ -7,21 +8,75 @@ namespace baconbaconbacon
 {
     public class baconbaconbaconModSystem : ModSystem
     {
-        // Called on server and client
-        // Useful for registering block/entity classes on both sides
+        private BaconConfig config;
+
         public override void Start(ICoreAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+            Mod.Logger.Notification("Bacon Bacon Bacon: " + api.Side + " started");
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("baconbaconbacon:hello"));
+            Mod.Logger.Notification("Bacon Bacon Bacon got itself up and running on the server. You're lucky it decided to get up off the couch...");
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("baconbaconbacon:hello"));
+            Mod.Logger.Notification("Bacon Bacon Bacon got itself up and running on your client. You're lucky it decided to get up off the couch...");
         }
+
+        public override void AssetsFinalize(ICoreAPI api)
+        {
+            // Set up Config
+            config = api.LoadModConfig<BaconConfig>("BaconBaconBaconConfig.json");
+            if (config == null)
+            {
+                config = new BaconConfig();
+            }
+            api.StoreModConfig(config, "BaconBaconBaconConfig.json");
+
+            // Disable Bacon Crop Stuff
+            var d1  = config.TurnOnBaconCrops != true;
+            var d2  = config.TurnOffBaconCrops != false;
+            var d3  = config.DefinitelyTurnOffBaconCrops != false;
+            var d4  = config.GrowableBacon != true;
+            var d5  = config.DoNotUseBaconCrops != false;
+            var d6  = config.TurnOffFun != false;
+            var d7  = config.TurnOffWhimsey != false;
+            var d8  = config.TurnOnChildlikeWondermentRecapturedAsAnAdult != true;
+            var d9  = config.SetThisValueToThePasswordOnTheModPageIfYouWantToTurnOffGrowableBacon == "FlankSteak";
+            var d10 = config.SeriouslyActuallyTurnOffTheWeirdBaconCrops != false;
+            var d11 = config.ISincerelyDontWantTheCabbageBacon != false;
+            var d12 = config.AreYouGettingTiredOfThisYetBecauseIfSoSetThisOneToTrue != false;
+            var d13 = config.GrowingBaconIsLame != false;
+            var d14 = config.TurnOnHumor != true;
+            var d15 = config.SetThisToTheAnswerToLifeTheUniverseAndEverythingToTurnOffBaconCrops == 42;
+            var d16 = config.MeatableCrops != true;
+            var d17 = config.CroppableMeats != true;
+            var d18 = config.BaconFarmingMechanics != true;
+            var d19 = config.SlavishDevotionToRealism != false;
+            var d20 = config.SetThisToTheIntegerNumberOneThousandTwoHundredAndTwentyFiveToTurnOffBaconCrops == 1225;
+            if (d1 && d2 && d3 && d4 && d5 && d6 && d7 && d8 && d9 && d10 && d11 && d12 && d13 && d14 && d15 && d16 && d17 && d18 && d19 && d20)
+            {
+                var items = api.World.Items.Where(i => i.Code?.Path != null
+                    && (i.Code.Path.ToLower().Contains("baconcrop")
+                    || i.Code.Path.ToLower().Contains("headofbacon")
+                    || i.Code.Path.ToLower().Contains("infusedbaconwrappedomokpiece")))
+                    .ToList();
+                foreach (var i in items)
+                    api.World.Items.Remove(i);
+
+                var recipes = api.World.GridRecipes.Where(r => (r.Output.Code?.Path != null && r.Output.Code.Path.ToLower().Contains("infusedbaconwrappedomokpiece"))
+                    || (r.Ingredients != null && r.Ingredients.Any(i => i.Value.Code.Path.Contains("headofbacon"))))
+                    .ToList();
+                foreach (var r in recipes)
+                    api.World.GridRecipes.Remove(r);
+
+                var blocks = api.World.Blocks.Where(b => b.Code?.Path != null && b.Code.Path.ToLower().Contains("baconcrop"));
+                foreach (var b in blocks)
+                    b.CreativeInventoryTabs = null;
+            }
+        }
+
     }
 }
